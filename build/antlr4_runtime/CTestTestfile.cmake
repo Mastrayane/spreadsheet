0c592a4a4ee120f96a9a4f969265d07abdbe050a
Indemion <mastrayane@mail.ru>
2024-09-03 23:07:57 +0300

    Обновлен .gitignore, добавлены папки build и dist

---
+++ build/antlr4_runtime/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/Repositories/spreadsheet/antlr4_runtime
+# Build directory: D:/Repositories/spreadsheet/build/antlr4_runtime
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("runtime")
---
+++ build/antlr4_runtime/runtime/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/Repositories/spreadsheet/antlr4_runtime/runtime
+# Build directory: D:/Repositories/spreadsheet/build/antlr4_runtime/runtime
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("D:/Repositories/spreadsheet/build/antlr4_runtime/runtime/antlr4_tests[1]_include.cmake")
+subdirs("../../_deps/googletest-build")
